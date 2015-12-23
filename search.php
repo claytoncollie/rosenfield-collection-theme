537023d88dda950680193a88e8c5eb4d982d525d
Clayton Collie <clayton.collie@gmail.com>
2015-12-23 08:51:05 -0600

    clean up search results page, remove blank index.php file

--- search.php
+++ search.php
@@ -14,9 +14,6 @@ function rc_search_genesis_meta() {
 	
 	// Add widget area on all pages just after header
 	add_action( 'genesis_after_header', 'rc_search_form_widget');
-	
-	// Add artist image to search result, just like regular post
-	//add_action( 'genesis_entry_header', 'rc_search_artist_image');
 
 }
 
@@ -33,20 +30,6 @@ function rc_search_form_widget() {
 			'before' => '<div class="search-form"><div class="wrap">',
 			'after'  => '</div></div>',
 		) );	
-}
-
-// Add widget area on all pages just after header
-function rc_search_artist_image() {
-	
-	$attachment_id = get_field( 'artist_photo', 'user_'.$id );
-	$size = "artist-image"; 
-	$author_avatar = wp_get_attachment_image_src( $attachment_id, $size ); 
-			
-	if($author_avatar) {
-		?>        
-        <a href="<?php echo $author_link; ?>" class="post-image entry-image" itemprop="image"><img src="<?php echo $author_avatar[0]; ?>" alt="<?php echo $author_first_name; ?> <?php echo $author_last_name; ?>" /></a>
-        <?php			
-	}
 }
 
 //* Run the Genesis loop
